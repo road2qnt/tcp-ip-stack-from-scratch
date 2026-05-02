2d0f977d5fd4e8e6cc3d4d1c4854767b44b6894e
leains <leonardusbrain0@gmail.com>
2026-05-02 17:17:51 +0700

    feat: menambahkan dasar dari cli

--- magi_system/cli.h
+++ magi_system/cli.h
@@ -1,3 +1,23 @@
-struct CLI{
+#ifndef CLI_H
+#define CLI_H
 
-};
+#include "core/interface.h"
+#include "core/link.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/*
+typedef struct CLI{
+    bool is_loop;
+} CLI;
+
+CLI cli_init();
+void process(CLI*cli)
+*/
+
+void cli();
+bool process(char* command);
+
+#endif
